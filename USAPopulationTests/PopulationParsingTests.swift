3d0d4c6dfcafab3685d0735828443e95b43d6043
Andrii Kvashuk <andremtcp2015a@gmail.com>
2024-03-15 22:01:41 +0000

    Ensure StatePopulation parses correctly with sample data

---
+++ USAPopulationTests/PopulationParsingTests.swift
@@ -0,0 +1,24 @@
+//
+//  PopulationParsingTests.swift
+//  USAPopulationTests
+//
+//  Created by Andrii Kvashuk on 15/03/2024.
+//
+
+import XCTest
+@testable import USAPopulation
+
+final class PopulationParsingTests: XCTestCase {
+
+    func testParsesStatePopulation() throws {
+        guard let sampleJSON = String.statePopulationJSON.data(using: .utf8) else {
+            throw NSError(domain: "JSON Parsing Test. Unable to convert sample json", code: 1)
+        }
+        
+        let response = try JSONDecoder().decode(StatePopulationResponse.self, from: sampleJSON)
+        
+        XCTAssertNotNil(response)
+        XCTAssertEqual(response.data.count, 52)
+    }
+
+}
