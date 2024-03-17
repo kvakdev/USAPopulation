6325bdfb7415dfe1d2adc00f812b4987c0bc565a
Andrii Kvashuk <andremtcp2015a@gmail.com>
2024-03-17 21:34:45 +0000

    Add UITabbarController instead of TabView

--- USAPopulation/AppFlowCoordinator.swift
+++ USAPopulation/AppFlowCoordinator.swift
@@ -12,6 +12,10 @@ class AppFlowCoordinator {
     let window: UIWindow
     let diContainer: DIContainer
     
+    //This is just to showcase how I would handle navigation from the coordinator,
+    let stateNavController: UINavigationController = UINavigationController()
+    let yearNavController: UINavigationController = UINavigationController()
+    
     init(window: UIWindow, diContainer: DIContainer) {
         self.window = window
         self.diContainer = diContainer
@@ -20,23 +24,27 @@ class AppFlowCoordinator {
     func start() {
         let loader = diContainer.makePopulationAPI()
 
-        window.rootViewController = UIHostingController(rootView:
-                                                            HomeTabView(viewModel: HomeTabViewModel(delegate: self),
-                                                                        makeLeftView:
-                                                                            NavigationStack {
-            StatePopulationView(viewModel: StatePopulationViewModel(api: loader, delegate: self))
-        },
-                                                                        makeRightView:
-                                                                            NavigationStack {
-            YearlyPopulationView(viewModel: YearlyPopulationViewModel(api: loader, delegate: self))
-        }
-            )
-                                                        
-        )
+        stateNavController.setViewControllers([
+            UIHostingController(rootView: StatePopulationView(viewModel: StatePopulationViewModel(api: loader, delegate: self)))
+        ], animated: false)
+        yearNavController.setViewControllers([
+            UIHostingController(rootView: YearlyPopulationView(viewModel: YearlyPopulationViewModel(api: loader, delegate: self)))
+        ], animated: false)
+        
+        let tabbarController = UITabBarController()
+        tabbarController.viewControllers = [
+            stateNavController,
+            yearNavController
+        ]
+        stateNavController.tabBarItem = UITabBarItem(title: "State", image: UIImage(systemName: "map.fill"), tag: 1)
+        stateNavController.navigationBar.prefersLargeTitles = true
+        yearNavController.tabBarItem = UITabBarItem(title: "Year", image: UIImage(systemName: "calendar"), tag: 2)
+        
+        window.rootViewController = tabbarController
+        
         window.makeKeyAndVisible()
     }
 }
 
-extension AppFlowCoordinator: HomeTabViewModelDelegate {}
 extension AppFlowCoordinator: StatePopulationViewModelDelegate {}
 extension AppFlowCoordinator: YearlyPopulationViewModelDelegate {}
